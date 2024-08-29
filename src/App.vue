783e5c0a54c71571932e5ebca619de1caeee5b10
manthosz <manthosz133@gmail.com>
2024-08-29 09:06:05 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+  import MyNev from './components/MyNev.vue'
+  import MyCard from './components/MyCard.vue'
+  import items from './test.json'
+</script>
+
+<template>
+<div>
+  <header>
+    <MyNev/>
+    <h1>TheWelcome</h1>
+    <div className="grid-container">
+    <MyCard v-bind="items[0]"/>
+    <MyCard v-bind="items[1]"/>
+    <MyCard v-bind="items[2]"/>
+    <MyCard v-bind="items[3]"/>
+  </div>
+    <!-- <MyCard/> -->
+    <!-- <MyCard/>
+    <MyCard/> -->
+    <!-- <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" /> 
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>-->
+  </header>
+
+  <main>
+    <!--<TheWelcome />-->
+  </main>
+</div>
+</template>
+
+<style scoped>
+p {
+  color: blue;
+  text-align: center;
+}
+.grid-container {
+  display: grid;
+  grid-column-gap: 0.5rem;
+  grid-row-gap: 0.5rem;
+}
+@media (min-width: 576px) {
+  .grid-container { grid-template-columns: repeat(1, 1fr); }
+}
+@media (min-width: 992px) {
+  .grid-container { grid-template-columns: repeat(3, 1fr); }
+}
+
+/* header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+} */
+</style>
---
+++ src/components/MyCard.vue
@@ -0,0 +1,56 @@
+<template lang="">
+    <div className="card">
+      <!-- <img src="https://img.freepik.com/premium-vector/vector-childrens-strange-adventures_1103584-155.jpg" alt="Avatar" style="width:100%">
+        <div class="container">
+          <h4><b>John Doe</b></h4> 
+          <p>Architect & Engineer</p> 
+        </div>
+
+      <img src="https://img.freepik.com/premium-vector/vector-childrens-strange-adventures_1103584-155.jpg" alt="Avatar" style="width:100%">
+        <div class="container">
+          <h4><b>view</b></h4> 
+          <p>Atmosphere pictures</p> 
+        </div>
+
+        <img src="https://img.freepik.com/premium-photo/free-beautiful-green-garden-cartoon-background-illustration-generative-ai_627719-5536.jpg" alt="Avatar" style="width:100%">
+        <div class="container">
+          <h4><b>John Doe</b></h4> 
+          <p>Architect & Engineer</p> 
+        </div> -->
+
+<img :src="image" alt="name" style="width:100%">
+      <div class="container">
+        <h4> <b> {{name}} </b></h4>
+        <p>{{nickname}}</p>
+        <p>{{tel}}</p>
+        <p>{{email}}</p>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['id','name','nickname','tel','email','image']
+}
+</script>
+<style scoped>
+    /* .card {
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  width: 40%;
+} */
+
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  width: 100%;
+}
+
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+
+.container {
+  padding: 2px 16px;
+}
+</style>
